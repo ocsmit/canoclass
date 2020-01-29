18b724073b7bad68c5acbea7d193869a829a97a3
ocsmit7654 <owen8461@protonmail.com>
2020-01-29 17:33:48 -0500

    Create process grid function

--- canopy_foss.py
+++ canopy_foss.py
@@ -1,105 +1,105 @@
-import os, sys
-import ogr
-from osgeo import gdal, ogr
+import os
+from osgeo import gdal, ogr, osr
 import numpy
-import json
 import sklearn
 from math import ceil
+import geopandas as gpd
 
 
-def seperate_qq(naip_qq, num_areas):
-    qq = naip_qq.GetLayer
-    naip_fields = json.loads(qq.ExportToJson())
+def create_join_process_naipqq_areas(naip_qq, process_areas, grid_size_deg, output_joined_file):
+    '''
+    This function creates a grid based on the extent of the input naip qq shapefile and
+    creates a new shapefile with the naip qq grid containing filenames and process area ID's
 
+    The size of each grid is measured in degrees.
+
+    Parameters
+    naip_qq = file path for naip qq shapefile
+    process_areas = file path for out put process area grid
+    grid_size_deg = size of each square in degrees
+    output_joined_file = file path for final joined output
+    '''
     # Get a Layer's Extent
-    inShapefile = "states.shp"
+    inShapefile = naip_qq
     inDriver = ogr.GetDriverByName("ESRI Shapefile")
     inDataSource = inDriver.Open(inShapefile, 0)
     inLayer = inDataSource.GetLayer()
+    srs = inLayer.GetSpatialRef()
     extent = inLayer.GetExtent()
 
-    # Create a Polygon from the extent tuple
-    ring = ogr.Geometry(ogr.wkbLinearRing)
-    ring.AddPoint(extent[0], extent[2])
-    ring.AddPoint(extent[1], extent[2])
-    ring.AddPoint(extent[1], extent[3])
-    ring.AddPoint(extent[0], extent[3])
-    ring.AddPoint(extent[0], extent[2])
-    poly = ogr.Geometry(ogr.wkbPolygon)
-    poly.AddGeometry(ring)
+    # convert sys.argv to float
+    xmin = float(extent[0])
+    xmax = float(extent[1])
+    ymin = float(extent[2])
+    ymax = float(extent[3])
+    gridWidth = float((grid_size_deg / 2) * 0.0625)
+    gridHeight = float((grid_size_deg / 2) * 0.0625)
 
-    # total number of blocks / num_areas = 'i'
-    # areas = math.round(i)
-    # if not field process_area_id exists:
-    #   create field
-    # if field process_area_id exists:
-    #   for j in naip_qq fields :
-    #       every areas fields input j + 1
+    # get rows
+    rows = ceil((ymax - ymin) / gridHeight)
+    # get columns
+    cols = ceil((xmax - xmin) / gridWidth)
 
-    # Fishnet gdal
-    def main(outputGridfn, xmin, xmax, ymin, ymax, gridHeight, gridWidth):
+    # start grid cell envelope
+    ringXleftOrigin = xmin
+    ringXrightOrigin = xmin + gridWidth
+    ringYtopOrigin = ymax
+    ringYbottomOrigin = ymax - gridHeight
 
-        # convert sys.argv to float
-        xmin = float(xmin)
-        xmax = float(xmax)
-        ymin = float(ymin)
-        ymax = float(ymax)
-        gridWidth = float(gridWidth)
-        gridHeight = float(gridHeight)
+    # create output file
+    outDriver = ogr.GetDriverByName('ESRI Shapefile')
+    if os.path.exists(process_areas):
+        os.remove(process_areas)
+    outDataSource = outDriver.CreateDataSource(process_areas)
+    outLayer = outDataSource.CreateLayer(process_areas, srs, geom_type=ogr.wkbPolygon)
+    featureDefn = outLayer.GetLayerDefn()
 
-        # get rows
-        rows = ceil((ymax - ymin) / gridHeight)
-        # get columns
-        cols = ceil((xmax - xmin) / gridWidth)
+    # create grid cells
+    countcols = 0
+    while countcols < cols:
+        countcols += 1
 
-        # start grid cell envelope
-        ringXleftOrigin = xmin
-        ringXrightOrigin = xmin + gridWidth
-        ringYtopOrigin = ymax
-        ringYbottomOrigin = ymax - gridHeight
+        # reset envelope for rows
+        ringYtop = ringYtopOrigin
+        ringYbottom = ringYbottomOrigin
+        countrows = 0
 
-        # create output file
-        outDriver = ogr.GetDriverByName('ESRI Shapefile')
-        if os.path.exists(outputGridfn):
-            os.remove(outputGridfn)
-        outDataSource = outDriver.CreateDataSource(outputGridfn)
-        outLayer = outDataSource.CreateLayer(outputGridfn, geom_type=ogr.wkbPolygon)
-        featureDefn = outLayer.GetLayerDefn()
+        while countrows < rows:
+            countrows += 1
+            ring = ogr.Geometry(ogr.wkbLinearRing)
+            ring.AddPoint(ringXleftOrigin, ringYtop)
+            ring.AddPoint(ringXrightOrigin, ringYtop)
+            ring.AddPoint(ringXrightOrigin, ringYbottom)
+            ring.AddPoint(ringXleftOrigin, ringYbottom)
+            ring.AddPoint(ringXleftOrigin, ringYtop)
+            poly = ogr.Geometry(ogr.wkbPolygon)
+            poly.AddGeometry(ring)
 
-        # create grid cells
-        countcols = 0
-        while countcols < cols:
-            countcols += 1
-
-            # reset envelope for rows
-            ringYtop = ringYtopOrigin
-            ringYbottom = ringYbottomOrigin
-            countrows = 0
-
-            while countrows < rows:
-                countrows += 1
-                ring = ogr.Geometry(ogr.wkbLinearRing)
-                ring.AddPoint(ringXleftOrigin, ringYtop)
-                ring.AddPoint(ringXrightOrigin, ringYtop)
-                ring.AddPoint(ringXrightOrigin, ringYbottom)
-                ring.AddPoint(ringXleftOrigin, ringYbottom)
-                ring.AddPoint(ringXleftOrigin, ringYtop)
-                poly = ogr.Geometry(ogr.wkbPolygon)
-                poly.AddGeometry(ring)
-
-                # add new geom to layer
-                outFeature = ogr.Feature(featureDefn)
-                outFeature.SetGeometry(poly)
-                outLayer.CreateFeature(outFeature)
-                outFeature = None
-
-                # new envelope for next poly
-                ringYtop = ringYtop - gridHeight
-                ringYbottom = ringYbottom - gridHeight
+            # add new geom to layer
+            outFeature = ogr.Feature(featureDefn)
+            outFeature.SetGeometry(poly)
+            outLayer.CreateFeature(outFeature)
+            outFeature = None
 
             # new envelope for next poly
-            ringXleftOrigin = ringXleftOrigin + gridWidth
-            ringXrightOrigin = ringXrightOrigin + gridWidth
+            ringYtop = ringYtop - gridHeight
+            ringYbottom = ringYbottom - gridHeight
 
-        # Save and close DataSources
-        outDataSource = None
+        # new envelope for next poly
+        ringXleftOrigin = ringXleftOrigin + gridWidth
+        ringXrightOrigin = ringXrightOrigin + gridWidth
+
+    # Save and close DataSources
+    outDataSource = None
+
+    naip = gpd.read_file(naip_qq)
+    areas = gpd.read_file(process_areas)
+
+    joined_naip = gpd.sjoin(naip, areas, how='inner', op='intersects')
+    joined_naip = joined_naip.rename(columns={'FID': 'PROCESS_ID'})
+    joined_naip = joined_naip[['FileName', 'PROCESS_ID', 'geometry']]
+    if os.path.exists(output_joined_file):
+        os.remove(output_joined_file)
+    joined_naip.to_file(output_joined_file)
+
+
