fe8cddd7fc133ccdd601d625b1d0836b86f4fb43
Owen Smith <owen8461@protonmail.com>
2020-01-29 09:07:34 -0500

    Start function to create processing regions

--- canopy_foss.py
+++ canopy_foss.py
@@ -2,5 +2,11 @@ import os
 import ogr
 import gdal
 import numpy
+import json
 import sklearn
 
+
+def seperate_qq(naip_qq, num_areas):
+  qq = naip_qq.GetLayer
+  naip_fields = json.loads(qq.ExportToJson())
+  
