6e5e7218c307c7834c77a5581c2744823697378d
Owen Smith <owen8461@protonmail.com>
2020-01-29 09:26:35 -0500

    Update canopy_foss.py

--- canopy_foss.py
+++ canopy_foss.py
@@ -10,3 +10,11 @@ def seperate_qq(naip_qq, num_areas):
   qq = naip_qq.GetLayer
   naip_fields = json.loads(qq.ExportToJson())
   
+  # total number of blocks / num_areas = 'i'
+  # if not field process_area_id exists:
+  #   create field
+  # if field process_area_id exists:
+  #   for j in naip_qq fields :
+  #       every i fields input j + 1
+  
+  
