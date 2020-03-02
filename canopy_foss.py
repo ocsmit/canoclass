2379e142067938961b703d928895f3ccede2bde3
ocsmit <owen8461@protonmail.com>
2020-03-02 14:33:49 -0500

    Add VDVI function

--- canopy_foss.py
+++ canopy_foss.py
@@ -226,6 +226,61 @@ def GRVI(naip_dir, out_dir):
     print('Finished')
 
 
+def VDVI(naip_dir, out_dir):
+
+    if not os.path.exists(naip_dir):
+        print('NAIP directory not found')
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+    gdal.UseExceptions()
+    gdal.AllRegister()
+    np.seterr(divide='ignore', invalid='ignore')
+
+    for dir, subdir, files in os.walk(naip_dir):
+        for f in files:
+            name = 'vdvi_%s' % f
+            if os.path.exists(os.path.join(out_dir, name)):
+                continue
+            if not os.path.exists(os.path.join(out_dir, name)):
+                if f.endswith('.tif'):
+                    # Open with gdal & create numpy arrays
+                    naip = gdal.Open(os.path.join(dir, f))
+                    red_band = naip.GetRasterBand(1).ReadAsArray(). \
+                        astype(np.float32)
+                    green_band = naip.GetRasterBand(2).ReadAsArray(). \
+                        astype(np.float32)
+                    blue_band = naip.GetRasterBand(3).ReadAsArray(). \
+                        astype(np.float32)
+                    snap = naip
+
+                    a = (2 * green_band - (red_band + blue_band))
+                    b = (2 * green_band + (red_band + blue_band))
+                    # Perform Calculation
+                    grvi = a / b
+
+                    # Save Raster
+                    driver = gdal.GetDriverByName('GTiff')
+                    metadata = driver.GetMetadata()
+                    shape = grvi.shape
+                    dst_ds = driver.Create(os.path.join(out_dir, name),
+                                           xsize=shape[1],
+                                           ysize=shape[0],
+                                           bands=1,
+                                           eType=gdal.GDT_Float32)
+                    proj = snap.GetProjection()
+                    geo = snap.GetGeoTransform()
+                    dst_ds.SetGeoTransform(geo)
+                    dst_ds.SetProjection(proj)
+                    dst_ds.GetRasterBand(1).WriteArray(grvi)
+                    dst_ds.FlushCache()
+                    dst_ds = None
+                    print(name)
+
+    print('Finished')
+
+
 def prepare_training_data(vector, ref_raster, out_raster, field='id'):
     """
     This function converts the training data shapefile into a raster to allow
@@ -300,7 +355,7 @@ def random_forests_class(training_raster, training_fit_raster, in_raster,
     X = n[t > 0]
     X = X.reshape(-1, 1)
 
-    clf = RandomForestClassifier(n_estimators=25, n_jobs=-1)
+    clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
     ras = clf.fit(X, y)
 
     r = gdal.Open(in_raster)
