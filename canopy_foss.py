1902343b0310faee74a7d017166173052206daf3
ocsmit <owen8461@protonmail.com>
2020-03-05 23:08:52 -0500

    Remove GRVI

--- canopy_foss.py
+++ canopy_foss.py
@@ -165,68 +165,6 @@ def nVARI(naip_dir, out_dir):
     print('Finished')
 
 
-def GRVI(naip_dir, out_dir):
-    """
-    This function walks through the input NAIP directory and performs the
-    GRVI calculation on each naip geotiff file and saves each new GRVI
-    geotiff in the output directory with the prefix 'grvi_'
-    ---
-    Args:
-        naip_dir: Folder which contains all subfolders of naip imagery
-        out_dir: Folder in which all calculated geotiff's are saved
-    """
-
-    if not os.path.exists(naip_dir):
-        print('NAIP directory not found')
-    if not os.path.exists(out_dir):
-        os.mkdir(out_dir)
-
-    gdal.PushErrorHandler('CPLQuietErrorHandler')
-    gdal.UseExceptions()
-    gdal.AllRegister()
-    np.seterr(divide='ignore', invalid='ignore')
-
-    for dir, subdir, files in os.walk(naip_dir):
-        for f in files:
-            name = 'grvi_%s' % f
-            if os.path.exists(os.path.join(out_dir, name)):
-                continue
-            if not os.path.exists(os.path.join(out_dir, name)):
-                if f.endswith('.tif'):
-                    # Open with gdal & create numpy arrays
-                    naip = gdal.Open(os.path.join(dir, f))
-                    red_band = naip.GetRasterBand(1).ReadAsArray(). \
-                        astype(np.float32)
-                    green_band = naip.GetRasterBand(2).ReadAsArray(). \
-                        astype(np.float32)
-                    snap = naip
-
-                    a = (green_band - red_band)
-                    b = (green_band + red_band)
-                    # Perform Calculation
-                    grvi = a / b
-
-                    # Save Raster
-                    driver = gdal.GetDriverByName('GTiff')
-                    metadata = driver.GetMetadata()
-                    shape = grvi.shape
-                    dst_ds = driver.Create(os.path.join(out_dir, name),
-                                           xsize=shape[1],
-                                           ysize=shape[0],
-                                           bands=1,
-                                           eType=gdal.GDT_Float32)
-                    proj = snap.GetProjection()
-                    geo = snap.GetGeoTransform()
-                    dst_ds.SetGeoTransform(geo)
-                    dst_ds.SetProjection(proj)
-                    dst_ds.GetRasterBand(1).WriteArray(grvi)
-                    dst_ds.FlushCache()
-                    dst_ds = None
-                    print(name)
-
-    print('Finished')
-
-
 def VDVI(naip_dir, out_dir):
 
     if not os.path.exists(naip_dir):
@@ -356,7 +294,7 @@ def random_forests_class(training_raster, training_fit_raster, in_raster,
 
     clf = RandomForestClassifier(n_estimators=50, n_jobs=-1,
                                max_features='sqrt',
-                               min_samples_leaf=100)
+                               min_samples_leaf=250, class_weight='balanced')
     ras = clf.fit(X, y)
 
     r = gdal.Open(in_raster)
@@ -408,8 +346,7 @@ def extra_random_forests_class(training_raster, training_fit_raster, in_raster,
 
     # TODO: Quantify min_samples_leaf threshold
     clf = ExtraTreesClassifier(n_estimators=50, n_jobs=-1,
-                               max_features='sqrt', min_samples_leaf=250,
-                               class_weight='balanced')
+                               max_features='sqrt', min_samples_leaf=10)
     ras = clf.fit(X, y)
 
     r = gdal.Open(in_raster)
