b1c70498e0eafe593a29ab9372403efe1bbec2c5
ocsmit <owen8461@protonmail.com>
2020-04-05 18:46:46 -0400

    Add variables

--- config-example.py
+++ config-example.py
@@ -1,8 +1,11 @@
-analysis_year = 2015
 workspace = '/folder_where_all_analysis_is_to_be_saved'
 naip_dir = '/input/path/to/naip/directory'
+arvi_dir = '%s/input_path_where_ARVI_to_be_saved' % workspace
 
-arvi_dir = '%s/%d/input_path_where_ARVI_to_be_saved' % (workspace, analysis_year)
-naipqq_shp = 'naipqq/shapefile/with/phyregs/*.shp'
-
+data = 'Data folder/'
+phyreg_lyr = '%s/*.shp'
+naipqq_shp = '%s/*.shp'
+training_shp = '%/training_shp.shp'
+training_raster = '%/training_raster.tif'
+reference_raster = '%s/NAIP raster data was drawn over.tif'
 
