90fc647e72854d576cce2ebd2ba1b00bb45deaa7
ocsmit <owen8461@protonmail.com>
2020-04-05 15:50:16 -0400

    Create config file
    WIP: Other functions need to be refactored to use

---
+++ canopy_foss/config-example.py
@@ -0,0 +1,8 @@
+analysis_year = 2015
+workspace = '/folder_where_all_analysis_is_to_be_saved'
+naip_dir = '/input/path/to/naip/directory'
+
+arvi_dir = '%s/%d/input_path_where_ARVI_to_be_saved' % (workspace, analysis_year)
+naipqq_shp = 'naipqq/shapefile/with/phyregs/*.shp'
+
+
