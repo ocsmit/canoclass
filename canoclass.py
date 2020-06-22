313601f2a56166f28ca90a7014a6f70882bca4db
ocsmit <owen8461@protonmail.com>
2020-06-22 00:00:42 -0400

    Verify change to processing regions works as intended

--- canoclass.py
+++ canoclass.py
@@ -331,7 +331,7 @@ def mosaic_tiles(pid):
         file = filtered[i]
         filename = '%s.tif' % file[:-13]
         in_file = '%s/%s%s' % (dir_path, 'cl_c_arvi_', filename)
-        out_file = '%s/%s%s.tif' % (dir_path, 'mosaic_', pid)
+        out_file = '%s/%s%s.tif' % (dir_path, 'mosaic_', str(pid))
         inputs.append(in_file)
         # Check if input file exists
         if not os.path.exists(inputs[i]):
@@ -339,16 +339,17 @@ def mosaic_tiles(pid):
             continue
 
     inputs_string = " ".join(inputs)
-    gdal_merge = "gdal_merge.py -co NBITS=2 -n 3\
-                 -init 3 -o %s -of gtiff %s" % (
+    print(inputs_string)
+    gdal_merge = "gdal_merge.py -co NBITS=2 -n 3 -init 3 -o %s -of gtiff %s" % (
         out_file, inputs_string)
     os.system(gdal_merge)
 
 
 def clip_mosaic(pid):
-    shp = config.phyreg_lyr
+    shp = config.proc_region
     results_dir = config.results
     proj = config.proj
+    id_field = config.procid_field
 
     region_dir = '%s/%s' % (results_dir, pid)
     dir_path = '%s/Outputs' % (region_dir)
@@ -356,7 +357,7 @@ def clip_mosaic(pid):
     in_raster = '%s/%s' % (dir_path, input_raster_name)
     out_raster = '%s/clipped_%s' % (dir_path, input_raster_name)
 
-    where = "PHYSIO_ID = %d" % pid
+    where = "%s = %d" % (id_field, pid)
 
     warp = gdal.Warp(out_raster, in_raster, xRes=1, yRes=1, cutlineDSName=shp,
                      cutlineWhere=where, cropToCutline=True,
