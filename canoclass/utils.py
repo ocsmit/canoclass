60f30d45f3cb9ab14bcd8adbc53b1b561570bd70
ocsmit <owen8461@protonmail.com>
2020-07-03 20:55:52 -0400

    Refactor to seperate load data to own function

--- canoclass/utils.py
+++ canoclass/utils.py
@@ -5,6 +5,22 @@ from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 
 
+def load_data(training_raster, training_fit_raster):
+
+    y_raster = gdal.Open(training_raster)
+    t = y_raster.GetRasterBand(1).ReadAsArray().astype(np.float64)
+    x_raster = gdal.Open(training_fit_raster)
+    n = x_raster.GetRasterBand(1).ReadAsArray().astype(np.float64)
+    n[np.isnan(n)] = 0
+    n_mask = np.ma.MaskedArray(n, mask=(n == 0))
+    n_mask.reshape(n.shape)
+    y = t[t > 0]
+    X = n_mask[t > 0]
+    X = X.reshape(-1, 1)
+
+    return X, y
+
+
 def prepare_training_data(input_shp, reference_raster, out_raster, field='id'):
     """
     This function converts the training data shapefile into a raster to allow
